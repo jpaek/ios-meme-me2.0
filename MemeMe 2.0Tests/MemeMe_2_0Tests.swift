ac34b4e8ff92e1b0f9d87ebfdf091ea48721af33
Jae Paek <jaehyun.paek@gmail.com>
2020-04-06 22:31:07 -0700

    Initial Commit

---
+++ MemeMe 2.0Tests/MemeMe_2_0Tests.swift
@@ -0,0 +1,34 @@
+//
+//  MemeMe_2_0Tests.swift
+//  MemeMe 2.0Tests
+//
+//  Created by Jae Paek on 4/6/20.
+//  Copyright © 2020 Jae Paek. All rights reserved.
+//
+
+import XCTest
+@testable import MemeMe_2_0
+
+class MemeMe_2_0Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
